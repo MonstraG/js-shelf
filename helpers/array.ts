f87e89b3c0232273e682eb3c3e15c9472b602669
ArsenyGarelyshev <monstrag@gmail.com>
2022-01-12 23:47:26 +0500

    Array helpers

--- helpers/array.ts
+++ helpers/array.ts
@@ -12,3 +12,56 @@ export const pop = <T>(
   }
   return [def, copy] as const;
 };
+
+
+export const sum = (array: number[]): number => array.reduce((acc, next) => acc + next, 0);
+export const max = (array: number[]): number => Math.max.apply(null, array);
+export const min = (array: number[]): number => Math.min.apply(null, array);
+export const avg = (array: number[]): number => sum(array) / array.length;
+
+export const newArr = (length: number) => Array.from({ length });
+
+export const unique = <T>(array: T[]): T[] =>
+  array.filter((item: T, index: number) => array.indexOf(item) == index);
+
+//splits array into 2 parts based on predicate, first corresponds to true, second to false
+export const fork = <T>(
+  array: T[],
+  predicate: (element: T, index: number, array: T[]) => boolean
+): T[][] =>
+  array.reduce(
+    ([truthful, falseful], element, ...props) => {
+      (predicate(element, ...props) ? truthful : falseful).push(element);
+      return [truthful, falseful];
+    },
+    [[], []]
+  );
+
+// https://stackoverflow.com/a/65184754
+type ObjectKey = string | number | symbol;
+export const groupBy = <K extends ObjectKey, TItem extends Record<K, ObjectKey>>(
+  items: TItem[],
+  key: K
+): Record<ObjectKey, TItem[]> =>
+  items.reduce(
+    (result, item) => ({
+      ...result,
+      [item[key]]: [...(result[item[key]] || []), item]
+    }),
+    {} as Record<ObjectKey, TItem[]>
+  );
+
+
+
+// zip([1,2,3], [a,b,c]) => [[1, a], [2, b], [3, c]]
+export const zip = <T1,T2>(array: T1[], array2: T2[]): [T1, T2][] => array.map((el, index) => [el, array2[index]])
+
+// zipShort([1,2], [a,b,c]) => [[1, a], [2, b]]
+export const zipShort = <T1,T2>(array: T1[], array2: T2[]): [T1, T2][] => {
+  const minLength = Math.min(array.length, array2.length);
+  return array.slice(0, minLength).map((el, index) => [el, array2[index]])
+}
+
+export const compareAsc = (a: number, b: number): number => a - b;
+export const compareDesc = (a: number, b: number): number => b - a;
+
