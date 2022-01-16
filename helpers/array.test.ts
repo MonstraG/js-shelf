0f99f74826de73c7ab7ee44be4e844a15598615a
ArsenyGarelyshev <monstrag@gmail.com>
2022-01-17 01:01:21 +0500

    # one more test, enabled and run prettier

--- helpers/array.test.ts
+++ helpers/array.test.ts
@@ -1,7 +1,6 @@
 import { Arr } from "./array";
 
-describe('sum', function () {
-  it("sums argument array", () => {
-    expect(Arr.sum([1,2,3,4])).toBe(10);
-  })
+describe("sum", function () {
+	it("sums argument array", () => expect(Arr.sum([1, 2, 3, 4])).toBe(10));
+	it("returns 0 on empty array", () => expect(Arr.sum([])).toBe(0));
 });
